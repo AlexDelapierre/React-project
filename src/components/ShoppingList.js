8cdda08070950f5d88b21a618fb038daa0c8485d
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-07-15 18:09:01 +0200

    refactor: update ShoppingList.js to handle discounted plants

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -14,9 +14,12 @@ function ShoppingList() {
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			<ul>
+			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
-					<li key={plant.id}>{plant.name}</li>
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.name}
+						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
+					</li>
 				))}
 			</ul>
 		</div>

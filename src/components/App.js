83a0a4a761a21d348086cfdb56fdb2ce0b479853
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-07-22 16:47:11 +0200

    refactor: Add state and props in App component

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,20 @@ import ShoppingList from './ShoppingList'
 import Footer from './Footer'
 
 function App() {
+	const [cart, updateCart] = useState([])
+	
 	return (
-		<div>
-			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>La maison jungle</h1>
-			</Banner>
-			<div className='lmj-layout-inner'>
-				<Cart />
-				<ShoppingList />
+			<div>
+					<Banner>
+							<img src={logo} alt='La maison jungle' className='lmj-logo' />
+							<h1 className='lmj-title'>La maison jungle</h1>
+					</Banner>
+					<div className='lmj-layout-inner'>
+							<Cart cart={cart} updateCart={updateCart} />
+							<ShoppingList cart={cart} updateCart={updateCart} />
+					</div>
+					<Footer />
 			</div>
-			<Footer />
-		</div>
 	)
 }
 
